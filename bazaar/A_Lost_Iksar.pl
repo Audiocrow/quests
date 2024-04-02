d7a1926c34ee0e097e3b98e05baf9e3931eba0d5
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-02 12:11:15 -0500

    update baz kunark npc

--- bazaar/A_Lost_Iksar.pl
+++ bazaar/A_Lost_Iksar.pl
@@ -13,7 +13,7 @@ sub EVENT_SAY {
             plugin::list_stage_prereq($client, 'RoK');            
         }
         if (($text =~/explorer/i) && ($expansion <2)){
-            plugin::Whisper("You are one of patience, I see. All you need to do is bring me an Apocryphal Elemental Binder, an Apocryphal Djarn's Amethyst Ring, an Apocryphal Crown of the Froglok Kings, and an Apocryphal Scalp of the Ghoul Lord. This will grant you three Apocryphal tokens. When one is turned in to me, that hero will be granted access to The Ruins of Kunark.");
+            plugin::NPCTell("You are one of patience, I see. All you need to do is bring me an Apocryphal Elemental Binder, an Apocryphal Djarn's Amethyst Ring, an Apocryphal Crown of the Froglok Kings, and an Apocryphal Scalp of the Ghoul Lord. This will grant you three Apocryphal tokens. When one is turned in to me, that hero will be granted access to The Ruins of Kunark.");
         }
     }
 }
@@ -22,26 +22,24 @@ sub EVENT_ITEM {
     my $test = quest::get_rule("Custom:MulticlassingEnabled");
     quest::debug("Rule Debug: $test");
     if (!plugin::is_stage_complete($client, 'RoK')) {
+        if (plugin::check_handin_fixed(\%itemcount, 2028043 => 1, 2010366 => 1, 2010142 => 1, 2026997 => 1)) {
+            set_subflag($client, 'RoK', 'Lord Nagafen', 1);
+            set_subflag($client, 'RoK', 'Lady Vox', 1);
+            quest::ding();
+            quest::exp(100000);
+
+            if (quest::get_rule("Custom:MulticlassingEnabled") neq "true") {
+                plugin::NPCTell("Here are two additional tokens for your companions to also gain access to the Ruins of Kunark");
+                quest::summonfixeditem(2019100);
+                quest::summonfixeditem(2019100);
+            }            
+        } elsif (plugin::check_handin_fixed(\%itemcount, 2019100 => 1)) {
+            set_subflag($client, 'RoK', 'Lord Nagafen', 1);
+            set_subflag($client, 'RoK', 'Lady Vox', 1);
+            quest::ding();
+            quest::exp(100000);
+        }
     }
 
     plugin::return_items(\%itemcount);
-
-    if ($expansion < 20){
-        if (plugin::check_handin_fixed(\%itemcount, 2028043 => 1, 2010366 => 1, 2010142 => 1, 2026997 => 1)) {
-		    plugin::Whisper("Here are three tokens. Hand them back to me for your flag!");
-            quest::ding();
-            quest::exp(100000);
-            quest::summonfixeditem(2019100);
-            quest::summonfixeditem(2019100);
-            quest::summonfixeditem(2019100);
-        }
-        if (plugin::check_handin_fixed(\%itemcount, 2019100 => 1)) {
-		    plugin::Whisper("Beware of the evils that lurk Kunark $name!");
-            quest::ding();
-            quest::set_data($key, 2);
-            quest::set_data($nagkey, 1);
-            quest::set_data($voxkey, 1);
-	    }
-	    #:: Return unused items
-    }
 }
