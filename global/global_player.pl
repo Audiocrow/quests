1f6f7fc5945f4a66ddd6b69a14d6f1d2425cc2ef
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-02 00:28:23 -0500

    remove a bunch of player scripts we dont need anymore

--- global/global_player.pl
+++ global/global_player.pl
@@ -5,29 +5,29 @@ sub EVENT_SIGNAL {
 
 sub EVENT_ENTERZONE { 
 	plugin::CommonCharacterUpdate($client);    
-
-	#if (!plugin::is_eligible_for_zone($client, $zonesn)) {
-	#	$client->Message(4, "Your vision blurs. You lose conciousness and wake up in a familiar place.");
-	#	$client->MovePC(151, 185, -835, 4, 390); # Bazaar Safe Location.
-	#}
+	if (!plugin::is_eligible_for_zone($client, $zonesn)) {
+		$client->Message(4, "Your vision blurs. You lose conciousness and wake up in a familiar place.");
+		$client->MovePC(151, 185, -835, 4, 390); # Bazaar Safe Location.
+	}
 }
 
 sub EVENT_CONNECT {
     plugin::CommonCharacterUpdate($client);
+	if (!plugin::is_eligible_for_zone($client, $zonesn)) {
+		$client->Message(4, "Your vision blurs. You lose conciousness and wake up in a familiar place.");
+		$client->MovePC(151, 185, -835, 4, 390); # Bazaar Safe Location.
+	}
 
     if (!$client->GetBucket("First-Login")) {
         $client->SetBucket("First-Login", 1);
+		$client->SummonItem(18471); #A Faded Writ
+        $client->Message(263, "You find a small note in your pocket.");
+		$client->SetBucket('FirstLogin', 1);
 
         my $name = $client->GetCleanName();
         my $full_class_name = plugin::GetPrettyClassString($client);
 
         plugin::WorldAnnounce("$name ($full_class_name) has logged in for the first time.");
-    }
-
-    if (!$client->GetBucket('FirstLogin')) {
-        $client->SummonItem(18471); #A Faded Writ
-        $client->Message(263, "You find a small note in your pocket.");
-		$client->SetBucket('FirstLogin', 1);
     }
 }
 
@@ -80,16 +80,13 @@ sub EVENT_ZONE {
     }     
 
     if (!plugin::is_eligible_for_zone($client, quest::GetZoneShortName($target_zone_id))) {
-		return int($from_zone_id);
         if ($from_zone_id == 151 && ($target_zone_id == 152 || $target_zone_id == 150)) {
-            my $BindX = $client->GetBindX();
-            my $BindY = $client->GetBindY();
-            my $BindZ = $client->GetBindZ();
-            my $BindH = $client->GetBindHeading();
-            my $BindZone = $client->GetBindZoneID();
-
-            $client->MovePC($BindZone, $BindX, $BindY, $BindZ, $BindH); # Bind Point
-            return int($BindZone);
+            $client->MovePC($client->GetBindZoneID(), 
+							$client->GetBindX(), 
+							$client->GetBindY(),
+							$client->GetBindZ(), 
+							$client->GetBindHeading()); # Bind Point
+            return int($client->GetBindZoneID());
         } else {
             $client->MovePC(151, 185, -835, 4, 390); # Bazaar Safe Location.
             return int(151);
