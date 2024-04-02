b9c5704c55514daf2b08340c49ec0d7047f9eefa
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-02 12:49:17 -0500

    test

--- bazaar/A_Planar_Projection.pl
+++ bazaar/A_Planar_Projection.pl
@@ -1,88 +1,60 @@
-sub EVENT_SAY {
-    $key = $client->AccountID() . "-kunark-flag";
-    $expansion = quest::get_data($key);
-	
-    if ($text=~/hail/i) {
-        if ($expansion >= 19) {
-            plugin::Whisper("EXCEPTIONAL! It looks like you are ready to join us in the battle for the future of Planes of Power! Good luck to you, hero!");
-        }
+my $item1 = 17324;
+my $item2 = 2028708;
+my $item3 = 2026826;
+my $item4 = 2061227;
 
-        if ($expansion < 19) {
-            plugin::Whisper("Ah... I see you have yet to unlock the Planes of Power. You have two options. One is the route of the [hero]. The other, is the route of the [collector].");
-    
+my $stage_desc = "Planes of Power";
+my $hero_desc = "To enter the Planes of Power, you must slay the Crawling Beast, the Master of End, the Creature of Nightmares, the Giver of Hate, and the Mighty Emperor.";
+
+sub EVENT_SAY {
+    if ($text=~/hail/i){
+        if (plugin::is_stage_complete($client, 'RoK')) {
+            plugin::YellowText("You have access to the $stage_desc.");
+        } else {
+            plugin::NPCTell("To gain access to the $stage_desc, two paths lie before you; [hero] and [explorer].");
         }
     }
-            if (($text =~/hero/i) && ($expansion <19)){
-            plugin::Whisper("To enter the Planes of Power, you must slay the Crawling Beast, the Master of End, the Creature of Nightmares, the Giver of Hate, and the Mighty Emperor.");
-            $progressionCount = 5;
-		    $progressCount = 0;
-            $progressText = "";
-            if (quest::get_data($client->AccountID() . "deep") > 0) {
-            $progressCount++;
-            $progressText .= "Thought Horror Fiend in the Deep, ";
-            }
-
-            if (quest::get_data($client->AccountID() . "akh") > 0) {
-            $progressCount++;
-            $progressText .= "The Insanity Crawler in Akheva Ruins, ";
-            }
-
-            if (quest::get_data($client->AccountID() . "griegs") > 0) {
-            $progressCount++;
-            $progressText .= "Greg Veneficus in Grieg's End, ";
-            }
-
-            if (quest::get_data($client->AccountID() . "ssraone") > 0) {
-            $progressCount++;
-            $progressText .= "Xerkizh the Creator in Ssraeshza Temple, ";
-            }
-
-            if (quest::get_data($client->AccountID() . "ssratwo") > 0) {
-            $progressCount++;
-            $progressText .= "Emperor Ssraeshza in Ssraeshza Temple, ";
-            }
-
-            if ($progressCount > 0) {
-            $progressText = substr($progressText, 0, -2);
-            plugin::Whisper("You have defeated $progressCount of $progressionCount targets: $progressText.");
-            }
+    elsif (!plugin::is_stage_complete($client, 'RoK')) {
+        if ($text =~/hero/i) {
+            plugin::NPCTell($hero_desc);
+            plugin::list_stage_prereq($client, 'RoK');            
         }
-        if (($text =~/collector/i) && ($expansion <19)){
-      plugin::Whisper("Yes, I remember how patient you were... Bring to me an Unadorned Scepter of Shadows, an Apocryphal Shadel Bandit Ring, an Apocryphal Zekhas' Katar, and an Apocryphal Blade of Insanity. This will grant you three Apocryphal tokens. When one is turned in to me, that hero will be granted access to The Planes of Power.");
-      return;
-      }
-  }
+        if (($text =~/explorer/i)){
+            my $item1_link = quest::varlink($item1);
+            my $item2_link = quest::varlink($item2);
+            my $item3_link = quest::varlink($item3);
+            my $item4_link = quest::varlink($item4);
+
+            my $response_string = "In that case, you will need to do is bring me the one each of following: [$item1_link], [$item2_link], [$item3_link], and [$item4_link].";
+            if (quest::get_rule("Custom:MulticlassingEnabled") ne "true") {
+                $response_string = $response_string . " Not only will I grant you access to the $stage_desc, but I will give you two tokens so that your companions can present them to me in order to also gain access.";
+            }
+
+            plugin::NPCTell($response_string);
+        }
+    }
+}
 
 sub EVENT_ITEM {
-  $key = $client->AccountID() . "-kunark-flag";
-  $expansion = quest::get_data($key);
+    if (!plugin::is_stage_complete($client, 'RoK')) {
+        if (plugin::check_handin_fixed(\%itemcount, 2028043 => 1, 2010366 => 1, 2010142 => 1, 2026997 => 1)) {
+            set_subflag($client, 'RoK', 'Lord Nagafen', 1);
+            set_subflag($client, 'RoK', 'Lady Vox', 1);
+            quest::ding();
+            quest::exp(100000);
 
-  if ($expansion < 20){
-    if (plugin::check_handin_fixed(\%itemcount, 17324 => 1, 2028708 => 1, 2026826 => 1, 2061227 => 1)) {
-      plugin::Whisper("Here are three tokens. Hand one back to me for your flag!");
-      quest::summonfixeditem(2019103);
-      quest::summonfixeditem(2019103);
-      quest::summonfixeditem(2019103);
-      quest::summonfixeditem(22198);
-
-      quest::ding();
-      quest::exp(1000000);
+            if (quest::get_rule("Custom:MulticlassingEnabled") ne "true") {
+                plugin::NPCTell("Here are two additional tokens for your companions to also gain access to the $stage_desc");
+                quest::summonfixeditem(2019103);
+                quest::summonfixeditem(2019103);
+            }            
+        } elsif (plugin::check_handin_fixed(\%itemcount, 2019103 => 1)) {
+            set_subflag($client, 'RoK', 'Lord Nagafen', 1);
+            set_subflag($client, 'RoK', 'Lady Vox', 1);
+            quest::ding();
+            quest::exp(100000);
+        }
     }
 
-  if ($expansion >= 14){
-    if (plugin::check_handin_fixed(\%itemcount, 2019103 => 1)) {
-      plugin::Whisper("Well done! Beware of the evils that lurk in the Planes $name!");
-      quest::ding();
-      quest::set_data($client->AccountID() . "akh", 1);
-      quest::set_data($client->AccountID() . "griegs", 1);
-      quest::set_data($client->AccountID() . "deep", 1);
-      quest::set_data($client->AccountID() . "ssraone", 1);
-      quest::set_data($client->AccountID() . "ssratwo", 1);
-
-      quest::set_data($key, 19);
-    }       
-  }
-  plugin::return_items(\%itemcount);
-  plugin::CheckCashPayment(0, $copper, $silver, $gold, $platinum);
-}
+    plugin::return_items(\%itemcount);
 }
